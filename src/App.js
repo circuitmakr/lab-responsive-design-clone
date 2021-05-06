369d5130335c1e0332e3f98198fe76694eeba09d
circuitmakr <circuitmakr@gmail.com>
2021-05-06 02:51:40 -0700

    responive site lab complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
 
-function App() {
+class App extends Component{
+  constructor(){
+    super()
+    this.state ={
+       showMenu: false
+    }
+  }
+  handleClick = ()=>{
+    this.setState({showMenu: !this.state.showMenu})
+  }
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="masthead">
+
+      <nav className="navbar">
+
+      <div className="navContainer">
+        <div className="nav-bar-brand-scroll-trigger">
+          <a href="#">
+            <img src={"https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg"} alt=''/>
+          </a>
+        </div>
+        <button className="navbar-toggler" onClick={this.handleClick}><a href='#'>Menu</a></button>
+     
+        <div className="navbar-responsive-collapse">
+        
+          <ul className="navbar-nav text-uppercase">
+            <li><a href="#">SERVICES</a></li>
+            <li><a href="#">PORTFOLIO</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">TEAM</a></li>
+            <li><a href="#">CONTACT</a></li>
+          </ul>
+        </div>
+      </div>
+      </nav>
+      {(this.state.showMenu === true)?
+      <ul className="navbar-toggler-menu">
+            <li><a href="#">SERVICES</a></li>
+            <li><a href="#">PORTFOLIO</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">TEAM</a></li>
+            <li><a href="#">CONTACT</a></li>
+          </ul>:<></>}
+
+      <div className="welcome-container">
+        <article className="masthead-sub-heading">Welcome to Our Studio!</article>
+        <article className="masthead-heading">It's Nice to Meet You</article>
+        <div className="btn-primary">Tell Me More</div>
+      </div>
       </header>
     </div>
   );
 }
-
+}
 export default App;
